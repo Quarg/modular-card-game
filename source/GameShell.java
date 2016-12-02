4a647fad44b36f944ecfe3e508cc03ba2e3c0470
Quarg <git@maxcd.org.uk>
2016-12-02 23:23:31 +0000

    Card Image Generation

--- source/GameShell.java
+++ source/GameShell.java
@@ -1,11 +1,15 @@
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import java.awt.Font;
+
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.Color;
+import org.newdawn.slick.TrueTypeFont;
 import org.newdawn.slick.SlickException;
 
 public class GameShell extends BasicGame
@@ -22,6 +26,9 @@ public class GameShell extends BasicGame
     protected Image[] image_deck_1;
     protected Image[] image_deck_2;
 
+    protected TrueTypeFont font_large;
+    protected TrueTypeFont font_small;
+
     public GameShell(String gamename)
     {
         super(gamename);
@@ -37,7 +44,7 @@ public class GameShell extends BasicGame
         deck[6] = Card.loadCardFromJSONFile("test_cards/unit/novice");
         deck[7] = Card.loadCardFromJSONFile("test_cards/unit/squire");
         deck[8] = Card.loadCardFromJSONFile("test_cards/unit/soldier");
-        deck[9] = deck[8];
+        deck[9] = Card.loadCardFromJSONFile("test_cards/unit/giant");
 
         testcard = deck[0];
 
@@ -52,6 +59,11 @@ public class GameShell extends BasicGame
 
         image_deck_1 = new Image[match.player_1.deck.length];
         image_deck_2 = new Image[match.player_2.deck.length];
+
+        Font font = new Font("Verdana", Font.BOLD, 32);
+        font_large = new TrueTypeFont(font, true);
+        font = new Font("Verdana", Font.BOLD, 16);
+        font_small = new TrueTypeFont(font, true);
 
         for (int i = match.player_1.deck.length - 1; i >= 0; i--) 
         {
@@ -70,12 +82,17 @@ public class GameShell extends BasicGame
     public void render(GameContainer gc, Graphics g) throws SlickException
     {
         //g.setBackground(Color.white);
-        g.drawString("Howdy!", 50, 50);
+        //g.drawString("Howdy!", 50, 50);
 
-        g.drawImage(image_deck_1[0],  50, 100);
-        g.drawImage(image_deck_1[3], 350, 100);
-        g.drawImage(image_deck_1[5], 650, 100);
-        g.drawImage(image_deck_1[8], 950, 100);
+        g.drawImage(image_deck_1[0],  50, 30);
+        g.drawImage(image_deck_1[3], 350, 30);
+        g.drawImage(image_deck_1[4], 650, 30);
+        g.drawImage(image_deck_1[5], 950, 30);
+
+        g.drawImage(image_deck_1[2],  50, 400);
+        g.drawImage(image_deck_1[7], 350, 400);
+        g.drawImage(image_deck_1[8], 650, 400);
+        g.drawImage(image_deck_1[9], 950, 400);
     }
 
     public Image createCardImage(Card card)
@@ -94,18 +111,19 @@ public class GameShell extends BasicGame
                 ig.drawImage(base_img_spell, 0, 0);
 
                 Ability ability = cardSpell.ability;
+                ig.setFont(font_small);
                 ig.setColor(Color.black);
 
                 int line = 0;
                 for (int selNum = 0; selNum < ability.selectorList.size(); selNum++) 
                 {
                     Selector sel = ability.selectorList.get(selNum);
-                    ig.drawString(sel.getString(selNum), 35, 155 + (line * 20));
+                    ig.drawString(sel.getString(selNum), 32, 155 + (line * 20));
                     line++;
                 }
                 for (Resolvable res : ability.resolvableList) 
                 {
-                    ig.drawString(res.getString(), 35, 155 + (line * 20));
+                    ig.drawString(res.getString(), 32, 155 + (line * 20));
                     line++;
                 }
             }
@@ -114,14 +132,40 @@ public class GameShell extends BasicGame
                 CardUnit cardUnit = (CardUnit) card;
                 ig.drawImage(base_img_unit, 0, 0);
 
-                ig.drawString(" "+cardUnit.attack+" ", 10, height - 35);
-                ig.drawString(" "+cardUnit.health+" ", width - 40, height - 35);
+                ig.setFont(font_large);
+                String var = Integer.toString(cardUnit.attack);
+                ig.drawString(var, 25 - (font_large.getWidth(var)/2f), 323 - (font_large.getHeight(var)/2f));
+                var = Integer.toString(cardUnit.health);
+                ig.drawString(var, 225 - (font_large.getWidth(var)/2f), 323 - (font_large.getHeight(var)/2f));
 
+                ig.setFont(font_small);
                 ig.setColor(Color.black);
+
+                int line = 0;
+                for (Ability ability : cardUnit.triggeredAbilities) 
+                {
+                    ig.drawString(ability.triggerCondition.triggerText, 32, 155 + (line * 20));
+                    line++;
+                    for (int selNum = 0; selNum < ability.selectorList.size(); selNum++) 
+                    {
+                        Selector sel = ability.selectorList.get(selNum);
+                        ig.drawString(sel.getString(selNum), 45, 155 + (line * 20));
+                        line++;
+                    }
+                    for (Resolvable res : ability.resolvableList) 
+                    {
+                        ig.drawString(res.getString(), 45, 155 + (line * 20));
+                        line++;
+                    }
+                }
+
             }
             ig.drawString(card.name, 65, 15);
+
+            ig.setFont(font_large);
             ig.setColor(Color.white);
-            ig.drawString(" "+card.evaluateCost()+" ", 10, 15);
+            String cost = Integer.toString(card.evaluateCost());
+            ig.drawString(cost, 25 - (font_large.getWidth(cost)/2f), 23 - (font_large.getHeight(cost)/2f));
             ig.flush();
             return image;
         }
